9429355d54cc523e5061653052ad4f3e80a43403
Tim Henderson <tim.tadh@gmail.com>
2015-02-02 20:51:31 -0500

    Added BpTree struct
    This the exported struct for the B+Tree. You can create a new tree or
    open an old tree.
    
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ bptree/bptree.go
@@ -0,0 +1,94 @@
+package bptree
+
+import (
+	"reflect"
+)
+
+import (
+	"github.com/timtadh/fs2/fmap"
+	"github.com/timtadh/fs2/slice"
+)
+
+type BpTree struct {
+	bf *fmap.BlockFile
+	metaBack []byte
+	meta *bpTreeMeta
+}
+
+type bpTreeMeta struct {
+	root uint64
+	keySize uint16
+}
+
+var bpTreeMetaSize uintptr
+
+func init() {
+	m := &bpTreeMeta{}
+	bpTreeMetaSize = reflect.TypeOf(*m).Size()
+}
+
+func newBpTreeMeta(bf *fmap.BlockFile, keySize uint16) ([]byte, *bpTreeMeta, error) {
+	a, err := bf.Allocate()
+	if err != nil {
+		return nil, nil, err
+	}
+	err = bf.Do(a, 1, func(bytes []byte) error {
+		_, err := newLeaf(bytes, keySize)
+		return err
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	data := make([]byte, bpTreeMetaSize)
+	meta := (*bpTreeMeta)(slice.AsSlice(&data).Array)
+	meta.root = a
+	meta.keySize = keySize
+	err = bf.SetControlData(data)
+	if err != nil {
+		return nil, nil, err
+	}
+	return data, meta, nil
+}
+
+func loadBpTreeMeta(bf *fmap.BlockFile) ([]byte, *bpTreeMeta, error) {
+	data, err := bf.ControlData()
+	if err != nil {
+		return nil, nil, err
+	}
+	meta := (*bpTreeMeta)(slice.AsSlice(&data).Array)
+	if meta.root == 0 || meta.keySize == 0 {
+		return nil, nil, Errorf("Meta was not properly initialized. Can't load tree")
+	}
+	return data, meta, nil
+}
+
+func New(bf *fmap.BlockFile, keySize int) (*BpTree, error) {
+	back, meta, err := newBpTreeMeta(bf, uint16(keySize))
+	if err != nil {
+		return nil, err
+	}
+	bpt := &BpTree{
+		bf: bf,
+		metaBack: back,
+		meta: meta,
+	}
+	return bpt, nil
+}
+
+func Open(bf *fmap.BlockFile) (*BpTree, error) {
+	back, meta, err := loadBpTreeMeta(bf)
+	if err != nil {
+		return nil, err
+	}
+	bpt := &BpTree{
+		bf: bf,
+		metaBack: back,
+		meta: meta,
+	}
+	return bpt, nil
+}
+
+func (b *BpTree) writeMeta() error {
+	return b.bf.SetControlData(b.metaBack)
+}
+
